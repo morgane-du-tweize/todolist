38826096c6741e3474640bb0f5a600fdecebf928
morgane-du-tweize <morgane_rt@hotmail.com>
2024-02-06 16:50:06 +0100

    updated

--- src/main/java/servlets/Details.java
+++ src/main/java/servlets/Details.java
@@ -10,12 +10,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import DAO.TacheDAO;
 import models.Tache;
 
 @WebServlet("/details")
 public class Details extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -23,13 +23,30 @@ public class Details extends HttpServlet {
 		if (session.getAttribute("connectedUser") == null) {
 			response.sendRedirect("connection");
 			return ;
-		}		
-		
-		Tache oneTask = (Tache) session.getAttribute("oneTask");
-		
-		System.out.println(oneTask);
+		}
 		request.getRequestDispatcher("/jsps/details.jsp").forward(request, response);
 	}
 
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		HttpSession session = request.getSession(true);
+		if (session.getAttribute("connectedUser") == null) {
+			response.sendRedirect("connection");
+			return ;
+		}
+		String idTache = request.getParameter("idtask");
+		Tache newTask = TacheDAO.selectById(idTache);
+		if (newTask == null) {
+			System.out.println("rien");
+		}
+		else {
+			request.setAttribute("idTache", newTask.getId());
+			request.setAttribute("dateLimite", newTask.getDateLimite());
+			request.setAttribute("description", newTask.getDescription());
+			doGet(request, response);			
+		}
+
+	}
 
 }
